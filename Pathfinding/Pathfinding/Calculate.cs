3b6d550f8cabe08f7e48eafa9f74833c48656f45
Daniel Korsah <dkorsah15@aol.co.uk>
2018-10-31 02:36:52 +0000

    Outline class for algorithm.

---
+++ Pathfinding/Pathfinding/Calculate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathfinding
+{
+    public static class Calculate
+    {
+        public static List<Node> Answer = new List<Node>();
+
+        public static void Dijkstra(List<Node> nodes)
+        {
+            Node start = nodes[0];
+            Node destination = nodes[nodes.Count-1];
+
+
+            Answer.Clear();
+        }
+    }
+}
