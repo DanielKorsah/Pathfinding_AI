64539de0ec9a1321c544e21cfb5b71aeaadcc853
Daniel Korsah <dkorsah15@aol.co.uk>
2018-11-01 01:28:30 +0000

    fix missing semicolon

--- Pathfinding/Pathfinding/Node.cs
+++ Pathfinding/Pathfinding/Node.cs
@@ -8,7 +8,7 @@ namespace Pathfinding
 {
     public class Node
     {
-        public int ID { get; private set }
+        public int ID { get; private set; }
         public int X { get; set; }
         public int Y { get; set; }
         public int[] Connections { get; private set; }
